03623aa61959071a3e427b3a623e16f4e2da0379
Matthew Enfield <21700581@bucks.ac.uk>
2019-04-11 13:01:05 +0100

    Added Quantity and total price - now have deleted PPU on shop for ex3.

--- confirmation.php
+++ confirmation.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
-   echo "<h2> Your order qty is ".$_SESSION['quantityValue']."</h2>";
-   echo "</br><h2> and the selected colour is $_POST[selcolour].</h2>"
+$quant = $_SESSION['quantityValue'];
+$val = $_SESSION['price'];
+$totalvalue = $quant*$val;
+   echo "<h2> Your order qty is ".$quant."</h2>";
+   echo "</br><h2> and the selected colour is $_POST[selcolour].</h2></br>";
+   echo "</br><h2> and the total price is $totalvalue.</h2></br>";
+
 ?>
--- selectcolour.php
+++ selectcolour.php
@@ -4,7 +4,9 @@
       <form action="confirmation.php"  method="post">
 	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
 <?php session_start();
-$_SESSION['quantityValue'] = $_POST[selqty]; ?>
+$_SESSION['quantityValue'] = $_POST[selqty];
+$_SESSION['price'] = $_POST[txtprice];?>
+
    	<select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>

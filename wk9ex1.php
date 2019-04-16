fa447a0a6f62615c2029cb585c75caacd77730e5
Matthew Enfield <21700581@bucks.ac.uk>
2019-04-16 19:59:33 +0100

    Update wk9ex1.php

--- wk9ex1.php
+++ wk9ex1.php
@@ -3,6 +3,21 @@
      echo "The lottery numbers for $lottodate are ";
      for($n=1;$n<7;$n++){
         $number[$n] = rand(1,49);
-        echo "<br/> $number[$n]";
-     }
+        echo "<br/> $number[$n]";}
+
+        $link = mysqli_connect("127.0.0.1", "root", "", "db1_21700581");
+
+        if (!$link) {
+            echo "Error: Unable to connect to MySQL." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+            exit;}
+
+  $sql = "insert into lotto (lottodate,number1,number2,";
+  $sql = $sql . "number3,number4,number5,number6)";
+  $sql = $sql . " values ($lottodate,$number[1],$number[2],";
+  $sql = $sql . "$number[3],$number[4],$number[5],$number[6]);";
+
+  $result = mysqli_query($link, $sql);
+  echo "<br/>This weeks numbers have been saved";
 ?>
